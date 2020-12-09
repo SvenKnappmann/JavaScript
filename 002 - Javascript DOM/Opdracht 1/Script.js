c46aaf626011279216822cddbccea9137eb1f10c
SvenKnappmann <SKnappmann@Roc-dev.com>
2020-12-09 14:06:11 +0100

    added H002/Opdracht 1 and H002/Opdracht 2

---
+++ 002 - Javascript DOM/Opdracht 1/Script.js
@@ -0,0 +1,30 @@
+var layout = {
+    titel:"Dit is de paginatitel",
+    subkop:"Dit is de subkop",
+    tekst: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. " +
+        "Aenean commodo ligula eget dolor. Aenean massa. " +
+        "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. " +
+        "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. " +
+        "Nulla consequat massa quis enim. " +
+        "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. " +
+        "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. " +
+        "Nullam dictum felis eu pede mollis pretium. Integer tincidunt. " +
+        "Cras dapibus. " +
+        "Vivamus elementum semper nisi. " +
+        "Aenean vulputate eleifend tellus. " +
+        "Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. " +
+        "Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. " +
+        "Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. " +
+        "Aenean imperdiet. " +
+        "Etiam ultricies nisi vel augue. " +
+        "Curabitur ullamcorper ultricies nisi. " +
+        "Nam eget dui.",
+
+}
+
+
+document.getElementById("titel").innerHTML = layout.titel
+document.getElementById("subkop").innerHTML = layout.subkop
+document.getElementById("tekst").innerHTML = layout.tekst
+document.getElementById("aap1").src = "img/monkey.png"
+document.getElementById("aap2").src = "img/monkey1.png"
---
+++ 002 - Javascript DOM/Opdracht 2/Script.js
@@ -0,0 +1,30 @@
+document.getElementById("body").style.backgroundColor = "white";
+document.getElementById("body").style.margin = "0";
+
+var blok1 = document.getElementById("blok1");
+blok1.style.width = "200px";
+blok1.style.height = "200px";
+blok1.style.backgroundColor = "skyblue";
+blok1.style.marginTop = "0";
+blok1.style.float = "left";
+
+var blok2 = document.getElementById("blok2")
+blok2.style.width = "200px";
+blok2.style.height = "200px";
+blok2.style.backgroundColor = "green";
+blok2.style.marginTop = "25px";
+blok2.style.float = "left";
+
+var blok3 = document.getElementById("blok3")
+blok3.style.width = "200px";
+blok3.style.height = "200px";
+blok3.style.backgroundColor = "yellow";
+blok3.style.marginTop = "65px";
+blok3.style.float = "left";
+
+var blok4 = document.getElementById("blok4")
+blok4.style.width = "200px";
+blok4.style.height = "200px";
+blok4.style.backgroundColor = "red";
+blok4.style.marginTop = "0";
+blok4.style.float = "left";
