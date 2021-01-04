3dc0777aa71617a9b8402e0fad3f797180ca4275
Sven <Svenn.knappmann@gmail.com>
2021-01-04 15:15:31 +0000

    added files

---
+++ 004 - Javascript Functies/Opdracht 2/JS.js
@@ -0,0 +1,16 @@
+var plaatjes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var teller = 0;
+var slideholder = document.getElementById("slideholder");
+slideholder.style.backgroundImage = "url('../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur1.png')"
+slideholder.addEventListener("click", function () {
+    getColor();
+    slideholder.style.backgroundImage = "url('../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur" + teller +".png')"
+});
+
+function getColor() {
+    if (teller >= plaatjes.length) {
+        teller = 0;
+    }
+    teller++;
+    return teller;
+}
