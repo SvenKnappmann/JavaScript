efc4a9fe31fcd927acfb3b2186edeb08e06a686a
Sven <Svenn.knappmann@gmail.com>
2021-01-05 10:20:22 +0000

    added files

--- 004 - Javascript Functies/Opdracht 2/JS.js
+++ 004 - Javascript Functies/Opdracht 2/JS.js
@@ -1,10 +1,10 @@
 var plaatjes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var teller = 0;
 var slideholder = document.getElementById("slideholder");
-slideholder.style.backgroundImage = "url('../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur1.png')"
+slideholder.style.backgroundImage = "url('../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur" + Math.floor(Math.random() * 9 + 1) + ".png')"
 slideholder.addEventListener("click", function () {
     getColor();
-    slideholder.style.backgroundImage = "url('../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur" + teller +".png')"
+    slideholder.style.backgroundImage = "url('../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur" + teller + ".png')"
 });
 
 function getColor() {
