6708abb991ad46b7943eb904f84892dd8e1b172c
Sven <Svenn.knappmann@gmail.com>
2021-01-05 14:18:45 +0000

    Ik kom niet verder doordat ik de groote van het hartje niet weet

---
+++ 005 - Javascript Interactief/Opdracht 1/JS.js
@@ -0,0 +1,34 @@
+pics = document.getElementById("pics");
+createPicsHolders();
+createColorImages()
+
+function createPicsHolders() {
+    for (let i = 0; i < 9; i++) {
+        pictureHolder = document.createElement("div");
+        pictureHolder.className = "picture-holder";
+        pictureHolder.id = "picture-holder-" + i;
+        pics.appendChild(pictureHolder);
+    }
+}
+
+function createColorImages() {
+    pictureHolders = document.getElementsByClassName("picture-holder");
+    for (let i = 0; i < pictureHolders.length; i++) {
+        favorite = document.createElement("div");
+        favorite.className = "favorite";
+        favorite.id = "favorite_" + (i + 1);
+        colorPicture = document.createElement("img");
+        colorPicture.src = "../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur" + (i + 1) + ".png";
+        colorPicture.id = (i + 1);
+        colorPicture.addEventListener("click", function() {
+            makeFav(this.id);
+        });
+        pictureHolders[i].appendChild(favorite);
+        pictureHolders[i].appendChild(colorPicture);
+    }
+}
+function makeFav(id) {
+    console.log("." + id);
+    favorite = document.getElementById("favorite_" + id);
+    favorite.style.backgroundImage = "url('../Img/New%20Piskel.png')";
+}
