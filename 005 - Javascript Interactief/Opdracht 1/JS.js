9a330f95e3b84ed9e8ea8a42e220febb70384dc9
Sven <Svenn.knappmann@gmail.com>
2021-01-07 11:42:57 +0000

    files changed

--- 005 - Javascript Interactief/Opdracht 1/JS.js
+++ 005 - Javascript Interactief/Opdracht 1/JS.js
@@ -20,15 +20,22 @@ function createColorImages() {
         colorPicture = document.createElement("img");
         colorPicture.src = "../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur" + (i + 1) + ".png";
         colorPicture.id = (i + 1);
-        colorPicture.addEventListener("click", function() {
+        colorPicture.addEventListener("click", function () {
             makeFav(this.id);
         });
         pictureHolders[i].appendChild(favorite);
         pictureHolders[i].appendChild(colorPicture);
     }
 }
+
 function makeFav(id) {
     console.log("." + id);
+    unfavorite = document.getElementsByClassName("favorite");
+
+    for (let i = 0; i < unfavorite.length; i++) {
+        unfavorite[i].style.backgroundImage = "none";
+    }
+
     favorite = document.getElementById("favorite_" + id);
     favorite.style.backgroundImage = "url('Img/New%20Piskel.png')";
 }
