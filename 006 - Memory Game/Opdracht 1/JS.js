b56ba396b369a9267c79dae4b804039319254faa
SvenKnappmann <Svenn.knappmann@gmail.com>
2021-01-11 11:26:26 +0100

    added files

--- 006 - Memory Game/Opdracht 1/JS.js
+++ 006 - Memory Game/Opdracht 1/JS.js
@@ -1,23 +1,23 @@
-playField = document.getElementById("playfield");
+playfield = document.getElementById("playfield");
 
 createPictureFrames();
-// getPictures();
+getPictures();
 
 function createPictureFrames() {
     for (let i = 0; i < 18; i++) {
         pictureHolder = document.createElement("div");
         pictureHolder.className = "picture-holder";
-        pictureHolder.id = "picture-holder-" + 1;
-        pics.appendChild(pictureHolder);
-
+        pictureHolder.id = "picture-holder-" + i;
+        playfield.appendChild(pictureHolder);
 
     }
 }
-// function getPictures() {
-//     for (let i = 0; i < 9; i++) {
-//         picture = document.createElement("img");
-//         picture.src = "../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur"+(i+1)+".png";
-//         picture.id = (i + 1);
-//         playField[i].appendChild(picture);
-//     }
-// }
+function getPictures() {
+    pictureHolder = document.getElementsByClassName("picture-holder");
+    for (let i = 0; i < pictureHolders.length; i++) {
+        picture = document.createElement("img");
+        picture.src = "../../003%20-%20Javascript%20Arrays/Opdracht%201/img/Kleur" + (i + 1) + ".png";
+        picture.id = (i + 1);
+        pictureHolder[i].appendChild(picture);
+    }
+}
