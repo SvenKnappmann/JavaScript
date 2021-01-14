9b85d137b5f2e87230f34050fdc262b75ea34be7
SvenKnappmann <Svenn.knappmann@gmail.com>
2021-01-14 14:14:26 +0100

    added files 0.6

--- 006 - Memory Game/Opdracht 1/JS.js
+++ 006 - Memory Game/Opdracht 1/JS.js
@@ -1,5 +1,5 @@
 let playfield = document.getElementById("playfield");
-let kaartPlaatjes = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
+let kaartPlaatjes = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
 let kaartPlaatjesGeheugen = [];
 KaartPlaatjesGeheugenvuller(kaartPlaatjesGeheugen);
 Shuffle(kaartPlaatjes);
@@ -8,49 +8,65 @@ createPictureFrames();
 Pictures();
 let counter = 0;
 let beurt = 0;
-let eerstePlaatje;
-let tweedePlaatje;
+let player1Score = 0;
+let player2Score = 0;
+let eersteKaartHouder;
+let tweedeKaartHouder;
+
 function createPictureFrames() {
     for (let i = 0; i < kaartPlaatjes.length; i++) {
         let pictureHolder = document.createElement("div");
         pictureHolder.className = "picture-holder";
-        pictureHolder.id = "picture-holder-" + i;
+        pictureHolder.id = i;
+        pictureHolder.addEventListener("click", function () {
+            Select(this.id);
+        });
         playfield.appendChild(pictureHolder);
     }
 }
+
 function Pictures() {
     let pictureHolders = document.getElementsByClassName("picture-holder");
     for (let i = 0; i < kaartPlaatjes.length; i++) {
         let picture = document.createElement("img");
-        picture.src = "img/Kleur" + kaartPlaatjes[i] + ".png";
         picture.id = kaartPlaatjes[i];
-        picture.addEventListener("click", function () {
-            Select(this.id);
-            console.log(kaartPlaatjesGeheugen.length);
-        });
+
         pictureHolders[i].appendChild(picture);
     }
 }
+
 function KaartPlaatjesGeheugenvuller(kaartPlaatjesGeheugen) {
     for (let i = 0; i < kaartPlaatjes.length; i++) {
         kaartPlaatjesGeheugen[i] = 0;
     }
 }
+
 function Shuffle(kaartPlaatjes) {
     kaartPlaatjes.sort(() => Math.random() - 0.5);
 }
+
 function Select(id) {
     counter++;
+    beurt++;
+    let pictureHolders = document.getElementsByClassName("picture-holder");
     if (counter % 2 === 1) {
-        eerstePlaatje = id;
+        eersteKaartHouder = id;
+        pictureHolders[id].firstChild.src = "img/Kleur" + kaartPlaatjes[id] + ".png";
         console.log("first");
     } else if (counter % 2 === 0) {
-        tweedePlaatje = id;
+        tweedeKaartHouder = id;
+        pictureHolders[id].firstChild.src = "img/Kleur" + kaartPlaatjes[id] + ".png";
         console.log("second");
-        if (eerstePlaatje === tweedePlaatje) {
+        if (kaartPlaatjes[eersteKaartHouder] === kaartPlaatjes[tweedeKaartHouder]) {
             console.log("correct");
-
+            kaartPlaatjesGeheugen[eersteKaartHouder] = 1;
+            kaartPlaatjesGeheugen[tweedeKaartHouder] = 1;
             //je mag nog een keer
+            if (beurt % 2 === 1) {
+                player1Score++;
+            } else if (beurt % 2 === 0) {
+                player2Score++;
+            }
         } else {
             console.log("incorrect");
             //beurt is over
